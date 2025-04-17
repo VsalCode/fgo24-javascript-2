26407dcc00a63e575fc200dd7deec84227e4c868
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-17 11:34:44 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,9 @@
+function oddEven(angkaX){
+  if(angkaX % 2 == 0){
+    console.log(angkaX + " adalah angka genap");
+  } else{
+    console.log(angkaX + " adalah angka ganjil");
+  }
+}
+
+oddEven(7)
