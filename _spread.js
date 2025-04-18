0a6f4d37d17f20e7c788b5079275fc86559b6aed
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-18 10:30:36 +0700

    Update _spread.js

--- _spread.js
+++ _spread.js
@@ -1,38 +1,67 @@
+/*--------------------------------------------------------------------------------- 
+CASE 1 :
+Menambah array "string" dengan berisikan "key" object yang sama seperti index ke-1, hanya beda value
+---------------------------------------------------------------------------------*/
 
-let data = [
-  ["nol",["nol","satu","dua",{ number: 123, string: ["random", { value: "Hello"}] }]], "lorem","ipsum"
+// cara destructuring dengan spread biasa : 
+
+const data = [
+  ["i",["i","i","i",{ number: 123, string: ["i", { value: "Hello"}] }]], "i","i"
 ]
 
+let { string } = data[0][1][3]
 
-data[0] = {
-  ...data[0][1][3].string[1],
-  value: "Hello Baru"
+string = [...string, { value: "Halo Baru !"}]
+
+console.log(string);
+
+
+// Nested Destructuring + spread :
+
+const data2 = [
+  ["i",["i","i","i",{ number: 123, string2: ["i", { value: "Hello"}] }]], "i","i"
+]
+
+let [[_b, [_c, _d, _e, { string2 }] ],_a] = data2
+
+string2 = [...string2, { value: "Hello Baru" } ]
+
+console.log(string2);
+
+
+/*---------------------------------------------------------------------------------
+CASE 2 :
+1. Menambah value "Hiking" di key "skills"
+2. Menambah object baru dengan value yang sama dengan object di index ke-0 di key education yang bertipe array 
+---------------------------------------------------------------------------------*/
+
+const bio = {
+  name: "John O'Connor",
+  age: 30
 }
 
-console.log(data[0]);
+const extraInfo = {
+  skills: ["web programming", "painting"],
+  education: [
+    {
+      name: "SMA 1 Denapasar",
+      year: 2022
+    }
+  ]
+}
 
+let { skills, education } = extraInfo
 
-  const bio = {
-    name: "John O'Connor",
-    age: 30
-  }
+skills = [...skills, "hiking"]
 
-  const extraInfo = {
-    skills: ["web programming", "painting"],
-    education: [
-      {
-        name: "SMP 1 Denapasar",
-        year: 2022
-      }
-    ]
-  }
+education = [ ...education ,{name: "SMA 2 DEPOK", year: 2025} ]
 
-  const profile = {
-    ...bio,
-    skills: ["hiking"],
-    ...extraInfo,
-    education: [{ name: "SMAN 2 DEPOK", year: 2023 }],
-  }
+const profile = {
+  ...bio,
+  ...extraInfo,
+  skills,
+  education
+}
 
-  console.log(profile);
-  
+console.log(profile);
+
