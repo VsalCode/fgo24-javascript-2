c4f93b85fd62d01f6264f03a8d8b54c44b89d0b1
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-18 10:46:58 +0700

    Update _destruct.js

--- _destruct.js
+++ _destruct.js
@@ -1,7 +1,20 @@
+// Akses Saja --------------------------------------------------------------------------
+
+const data = [
+  ["i",["i","i","i",{ number: 123, string: ["i", { value: "Hello"}] }]], "i","i"
+]
+
+const [[_b, [_c, _d, _e, { string: [_f,{ value }] }] ],_a] = data
+console.log(value);
+
+// Reassign --------------------------------------------------------------------------
+
 const data = [
   ["i",["i","i","i",{ number: 123, string: ["i", { value: "Hello"}] }]], "i","i"
 ]
 
 let [[_b, [_c, _d, _e, { string: [_f,{ value }] }] ],_a] = data
+
+value = "Hello Baru!"
 
 console.log(value);
