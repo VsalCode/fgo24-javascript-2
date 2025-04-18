93eaa112d459c93ac0130bdbe87db016778b84eb
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-18 10:43:45 +0700

    Add files via upload

---
+++ _destruct.js
@@ -0,0 +1,7 @@
+const data = [
+  ["i",["i","i","i",{ number: 123, string: ["i", { value: "Hello"}] }]], "i","i"
+]
+
+let [[_b, [_c, _d, _e, { string: [_f,{ value }] }] ],_a] = data
+
+console.log(value);
